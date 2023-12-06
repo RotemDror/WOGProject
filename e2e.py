5c27e739beb4aac85e13038cee1c880920649617
RotemDror <rotemdror98@gmail.com>
2023-12-06 02:35:49 +0200

    Add additional files

--- e2e.py
+++ e2e.py
@@ -1,11 +1,5 @@
-import requests
 import re
-from bs4 import BeautifulSoup
-import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 

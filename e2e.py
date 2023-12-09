e4df0e77561ee755af22ebbd69d6539c7fd0b633
RotemDror <rotemdror98@gmail.com>
2023-12-09 16:54:14 +0200

    Add additional files

--- e2e.py
+++ e2e.py
@@ -3,17 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 
 
 def test_scores_service(url):
 
     try:
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        web_driver = webdriver.Chrome(options=options, service=Service("./chromedriver"))
-        WebDriverWait(web_driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/p")))
+        web_driver = webdriver.Chrome(options=options)
         web_driver.get(url)
+        WebDriverWait(web_driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/p")))
         data = web_driver.find_element(By.XPATH, "/html/body/p")
         data = data.get_attribute('innerHTML')
 
